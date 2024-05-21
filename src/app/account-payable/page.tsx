6a1074da827e6ebf733c5e95b49cddf08e40c4ed
geraldkachi <fitzgeraldkachi@gmail.com>
2024-05-21 17:10:25 +0100

    navbar payable

--- src/app/account-payable/page.tsx
+++ src/app/account-payable/page.tsx
@@ -14,7 +14,7 @@ const AccountPayable = () => {
       <NavBar />
       <div className={`max-w-7xl mx-auto flex flex-row items-center justify-center gap-5`}>
         <div className="flex flex-col items-center my-7 md:my-14">
-          <div className="bg-[#01C467] text-white text-sm font-bold leading-5 rounded-3xl px-4 py-2 mb-5 w-max">PRICING</div>
+          <div className="bg-[#01C467] text-white text-sm font-bold leading-5 rounded-3xl px-4 py-2 mb-5 w-max">PAYABLE</div>
 
           <p className={`text-center mt-2 text-[#00000A] text-[32px] font-extrabold md:text-[56px] lg:leading-[81.2px]`}>
             Simplify Payments and <br /> <span className="text-[#01C467]">Manage</span> Vendor  Effortlessly.
