87a96557e591a64df1226c92d7d8707be9d42dc3
geraldkachi <fitzgeraldkachi@gmail.com>
2024-06-02 16:02:21 +0100

    email

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,15 +11,15 @@ const Page4041 = () => {
 
         {/* :ERROR 404 */}
         <div className="relative px-5 lg:border-r-2 border-gray-100">
-          <p className="absolute -top-3 md:top-0 left-10 md:left-20 text-base md:text-4xl text-indigo-600 font-bold uppercase">Error</p>
+          <p className="absolute -top-3 md:top-0 left-10 md:left-20 text-base md:text-4xl text-white font-bold uppercase">Error</p>
           <p className="text-9xl md:text-10xl text-white font-extrabold tracking-wider">404</p>
         </div>
 
         {/* :TEXT CONTAINER */}
-        <div className="px-5">
+        <div className="px-5 flex flex-col md:items-start items-center md:justify-start justify-center">
           <p className="text-3xl md:text-5xl text-white font-bold tracking-wide">Page not found</p>
           <p className="mt-4 text-sm md:text-base text-gray-500 font-medium">The content you’re looking for doesn’t exist. Either it was removed, or you mistyped the link. <br /><br />Sorry about that! Please visit our hompage to get where you need to go.</p>
-          <Link href='/' type="button" className="mt-10 relative inline-flex items-center px-7 py-3.5 rounded border border-transparent bg-[#01C467] md:text-lg text-white font-medium">
+          <Link href='/' type="button" className="mt-10 relative w-max inline-flex items-center px-7 py-3.5 rounded border border-transparent bg-white md:text-lg text-[#01C467] font-medium">
             Go back to Homepage
           </Link>
         </div>
