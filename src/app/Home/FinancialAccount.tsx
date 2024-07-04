cad2b4fb7e96d874826c3bb96ba38e3d592c0f9c
geraldkachi <fitzgeraldkachi@gmail.com>
2024-07-04 12:41:56 +0100

    latest

--- src/app/Home/FinancialAccount.tsx
+++ src/app/Home/FinancialAccount.tsx
@@ -49,36 +49,36 @@ const FinancialAccount = () => {
         </div>
       </section> */}
 
-      <div className=" w-full h-full items-start bg-inherit grid md:grid-cols-3 gap-4 pb-5">
+      <div className=" w-full h-full items-start bg-inherit grid md:grid-cols-3 gap-8 pb-5">
 
         <div className="mx-[unset] max-[0px]:pl-0 max-sm:pl-3 max-lg:pl-10 lg:ml-40 mt-14 col-span-2">
           <span className="rounded-full bg-[#00000A] px-4 py-3 max-w capitalize text-white text-sm tracking-[-1%] leading-[160%]">WHY CHOOSE US</span>
-          <div className="mt-5 text-2xl md:text-[40px] leading-[36px] md:leading-[64px] tracking-[-3%] md:font-medium font-bold">
+          <div className="mt-5 mb-10 md:mb-20 text-2xl md:text-[40px] leading-[36px] md:leading-[64px] tracking-[-3%] md:font-medium font-bold">
             Money-Tracking Headaches, <br />
             Finally Off Your Plate
           </div>
           
           <div className="">
-            <div className="md:col-span-2 grid sm:grid-cols-2 gap-4 h-max">
-              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 bg-[#FAFAFA]`}>
+            <div className="md:col-span-2 grid sm:grid-cols-2 gap-6 h-max">
+              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 rounded-[14px] bg-[#FAFAFA]`}>
                 <div>
                   <p className="text-[#00000A] mt-3 text-2xl font-medium md:font-semibold text-wrap my-2 mb-3 leading-[135%] md:tracking-[-4%]">Save Countless hours of work </p>
                   <p className="text-[#808084] text-base text-wrap font-normal leading-[180%] tracking-[-2%] md:tracking-[-1%]">Settle financial transactions in one click and go about your business as usual. No time wasted. </p>
                 </div>
               </motion.div>
-              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 bg-[#FFF8E5]`}>
+              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 rounded-[14px] bg-[#FFF8E5]`}>
                 <div>
                   <p className="text-[#00000A] mt-3 text-2xl font-medium md:font-semibold text-wrap my-2 mb-3 leading-[135%] md:tracking-[-4%]">Organise Your Finances All Year Round </p>
                   <p className="text-[#808084] text-base text-wrap font-normal leading-[180%] tracking-[-2%] md:tracking-[-1%]">Run your business confidently, knowing that no matter how busy it gets, your financial books will never again be a mess.</p>
                 </div>
               </motion.div>
-              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 bg-[#F8F5FF]`}>
+              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 rounded-[14px] bg-[#F8F5FF]`}>
                 <div>
                   <p className="text-[#00000A] mt-3 text-2xl font-medium md:font-semibold text-wrap my-2 mb-3 leading-[135%] md:tracking-[-4%]">Be Rest Assured of Security</p>
                   <p className="text-[#808084] text-base text-wrap font-normal leading-[180%] tracking-[-2%] md:tracking-[-1%]">Our platform is 100% safe from hackers and online scammers. We are PCI DSS Level 1, NDPR, and ISOs27001/2000&22301 certified.</p>
                 </div>
               </motion.div>
-              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 bg-[#E3FDF1]`}>
+              <motion.div variants={navVariants} initial="hidden" whileInView="show" className={`p-4 md:p-8 rounded-[14px] bg-[#E3FDF1]`}>
                 <div>
                   <p className="text-[#00000A] mt-3 text-2xl font-medium md:font-semibold text-wrap my-2 mb-3 leading-[135%] md:tracking-[-4%]">Get Instant Support When Needed</p>
                   <p className="text-[#808084] text-base text-wrap font-normal leading-[180%] tracking-[-2%] md:tracking-[-1%]">Need help or support at any point?  Reach us via email, help center or phone, and we&apos;ll be available to help you immediately.</p>
